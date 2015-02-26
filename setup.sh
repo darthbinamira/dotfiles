d8df4536dd49a32fbde9981cf7cc766de27d0116
Darth Binamira <dummymael@gmail.com>
2015-02-26 08:53:30 -0800

    Remove PPAs. Ubuntu default packages now point to the versions we want for terminator and tmux.

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@ BASEDIR=$(pwd)
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
-sudo add-apt-repository ppa:gnome-terminator
-sudo add-apt-repository ppa:pi-rho/dev
-
 sudo apt-get update
 sudo apt-get install -y git vim zsh curl wget tree terminator gnome-panel tmux python-pip lxc-docker
 sudo pip install -U fig
@@ -16,6 +13,7 @@ curl -L http://install.ohmyz.sh | sh
 
 # setup vim-pathogen
 git clone https://github.com/tpope/vim-pathogen.git ~/.vim-pathogen
+mkdir ~/.vim
 cd ~/.vim/
 ln -s ~/.vim-pathogen/autoload .
 mkdir -p ~/.vim/bundle
